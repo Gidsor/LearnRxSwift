c2c8a3f37b4518925704c9258c5612ec4db1fcd5
Vadim Denisov <gidsor@gmail.com>
2018-09-27 20:53:13 +0500

    Added SearchCity Example

---
+++ SearchCity/SearchCity/SearchTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SearchTableViewController.swift
+//  SearchCity
+//
+//  Created by Vadim Denisov on 27/09/2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SearchTableViewController: UITableViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var searchCity = [String]()
+    let cities = ["Tyumen", "Moscow", "Novosibirsk", "Yekaterinburg", "Omsk", "Samara", "Tver"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.rx
+            .text
+            .orEmpty
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .subscribe(onNext: { [unowned self] query in
+                self.searchCity = self.cities.filter { $0.hasPrefix(query) }
+                self.tableView.reloadData()
+            })
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchCity.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath)
+        cell.textLabel?.text = searchCity[indexPath.row]
+        return cell
+    }
+}
